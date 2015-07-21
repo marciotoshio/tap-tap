c5b19c56b4b5d8fd99666a1e527bf5f7aa8a1f8f
Marcio Toshio Ide <contato@marciotoshio.com.br>
2015-07-21 13:19:25 +0000

    first commit

---
+++ assets/game.js
@@ -0,0 +1,10 @@
+var game = new Phaser.Game(600, 480, Phaser.AUTO, 'gameCanvas', { preload: preload, create: create, update: update });
+
+function preload() {
+}
+
+function create() {
+}
+
+function update() {
+}
