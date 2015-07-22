e46417221d556201e623e6884ec42f7538a66ec6
Marcio Toshio Ide <contato@marciotoshio.com.br>
2015-07-22 17:21:08 +0000

    crosshair

--- assets/game.js
+++ assets/game.js
@@ -1,22 +1,31 @@
 var game = new Phaser.Game(600, 480, Phaser.AUTO, 'gameCanvas', { preload: preload, create: create, update: update });
 
-var explosion;
+var explosion, crosshair;
 
 function preload() {
   game.load.image('bg-wood', 'assets/images/bg-wood.jpg');
   game.load.image('bg-stone', 'assets/images/bg-stone.jpg');
   game.load.image('bg-cracks', 'assets/images/bg-cracks.jpg');
   game.load.image('explosion', 'assets/images/explosion.png');
+  game.load.image('crosshair', 'assets/images/crosshair.png');
 }
 
 function create() {
   game.add.sprite(0, 0, 'bg-wood');
+
+  crosshair = game.add.sprite(game.input.mousePointer.x, game.input.mousePointer.y, 'crosshair');
+  crosshair.anchor.setTo(.5, .5);
+  crosshair.scale.setTo(.1,.1);
+
   explosion = game.add.sprite(0, 0, 'explosion');
   explosion.kill();
+
   game.input.onDown.add(clickHandle, this);
 }
 
 function update() {
+  crosshair.x = game.input.mousePointer.x;
+  crosshair.y = game.input.mousePointer.y;
 }
 
 function clickHandle(pointer) {
